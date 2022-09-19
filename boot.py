83856647ba7e7edcf7d7e20740cf3698032d1992
Andrew Sheldrick <ajsheld12@gmail.com>
2022-09-19 00:30:16 -0400

    more robust setup

--- boot.py
+++ boot.py
@@ -1,24 +1,84 @@
-import bthid
 import time
-from machine import Pin
+import gc
+from machine import Pin, ADC, PWM
 from bthid import Joystick
 
 
 class Device:
     def __init__(self):
         self.joystick = Joystick("guitar")
-        self.buttons = {
-            "GREEN": (Pin(23, Pin.IN), self.button_green),
-            "RED": (Pin(22, Pin.IN), self.button_red),
-            "YELLOW": (Pin(21, Pin.IN), self.button_yellow),
-            "BLUE": (Pin(19, Pin.IN), self.button_blue),
-            "ORANGE": (Pin(18, Pin.IN), self.button_orange),
-            "START": (Pin(17, Pin.IN), self.button_start),
-            "SELECT": (Pin(16, Pin.IN), self.button_select)
-        }
+        self.joystick.load_secrets()
+        self.buttons = [
+            # Green
+            Pin(21, Pin.IN, Pin.PULL_UP),
+            # Red (broken)
+            Pin(23, Pin.IN, Pin.PULL_UP),
+            # Yellow
+            Pin(18, Pin.IN, Pin.PULL_UP),
+            # Blue
+            Pin(19, Pin.IN, Pin.PULL_UP),
+            # Orange
+            Pin(22, Pin.IN, Pin.PULL_UP),
+            # Start
+            Pin(17, Pin.IN, Pin.PULL_UP),
+            # Select
+            Pin(16, Pin.IN, Pin.PULL_UP),
+            # Star power
+            # Pin(27, Pin.IN, Pin.PULL_UP)
+        ]
+
+        # self.whammy = ADC(Pin(4))
+        self.strum_pins = (
+            Pin(5, Pin.IN, Pin.PULL_UP),
+            Pin(14, Pin.IN, Pin.PULL_UP)
+        )
+        self.strum_pos = 0
+
+        self.led = PWM(Pin(15))
+        self.buttonmap = [0, 0, 0, 0, 0, 0, 0]
         self.joystick.set_state_change_callback(self.joystick_state_callback)
         self.joystick.start()
         pass
+
+    def poll_buttons(self):
+        self.strum_pos = ((not self.strum_pins[0].value()) - (not self.strum_pins[1].value()))*127
+        # self.whammy_pos = self.whammy.read_u16()*(127/65535)
+
+        self.buttonmap = [
+            int(not self.buttons[0].value()),
+            int(not self.buttons[1].value()),
+            int(not self.buttons[2].value()),
+            int(not self.buttons[3].value()),
+            int(not self.buttons[4].value()),
+            int(not self.buttons[5].value()),
+            int(not self.buttons[6].value())
+            # int(not self.buttons[7].value())
+        ]
+        self.notify_buttonmap()
+
+    def notify_buttonmap(self):
+        self.joystick.set_buttons(
+            b1=self.buttonmap[0],
+            b2=self.buttonmap[1],
+            b3=self.buttonmap[2],
+            b4=self.buttonmap[3],
+            b5=self.buttonmap[4],
+            b6=self.buttonmap[5],
+            b7=self.buttonmap[6]
+            # b8=self.buttonmap[7]
+        )
+
+        self.joystick.set_axes(0, self.strum_pos) # self.whammy_pos, self.strum_pos)
+        try:
+            self.joystick.notify_hid_report()
+        except:
+            pass
+
+    def advertise(self):
+        self.joystick.start_advertising()
+        
+    def stop_advertise(self):
+        self.joystick.stop_advertising()
 
     def joystick_state_callback(self):
         if self.joystick.get_state() is Joystick.DEVICE_IDLE:
@@ -26,59 +86,17 @@ class Device:
         elif self.joystick.get_state() is Joystick.DEVICE_ADVERTISING:
             return
         elif self.joystick.get_state() is Joystick.DEVICE_CONNECTED:
+            gc.collect()
             return
         else:
             return
 
-    def advertise(self):
-        self.joystick.start_advertising()
-
-    def stop_advertising(self):
-        self.joystick.stop_advertising()
-
-    def button_green(self, val):
-        self.joystick.set_buttons(b1=val)
-        self.joystick.notify_hid_report()
-
-    def button_red(self, val):
-        self.joystick.set_buttons(b2=val)
-        self.joystick.notify_hid_report()
-
-    def button_yellow(self, val):
-        self.joystick.set_buttons(b3=val)
-        self.joystick.notify_hid_report()
-
-    def button_blue(self, val):
-        self.joystick.set_buttons(b4=val)
-        self.joystick.notify_hid_report()
-
-    def button_orange(self, val):
-        self.joystick.set_buttons(b5=val)
-        self.joystick.notify_hid_report()
-
-    def button_start(self, val):
-        self.joystick.set_buttons(b6=val)
-        self.joystick.notify_hid_report()
-
-    def button_select(self, val):
-        self.joystick.set_buttons(b7=val)
-        self.joystick.notify_hid_report()
-
     def start(self):
-        self.joystick.start_advertising()
+        self.advertise()
         while True:
-            # just an idea
-            buttonmap = {
-                'b1': 0,
-                'b2': 0,
-                'b3': 0,
-                'b4': 0,
-                'b5': 0,
-                'b6': 0,
-                'b7': 0
-            }
-            self.joystick.set_buttons(buttonmap)
-
+            if self.joystick.get_state() is Joystick.DEVICE_IDLE:
+                self.joystick.start_advertising()
+            self.poll_buttons()
 
 if __name__ == "__main__":
     d = Device()
